7f8b85c2c4de7e3b8da98f07e680cce006b91cbd
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-04 01:16:40 +0100

    grid layout for showing the dogs dynamic

--- src/Components/gridView.js
+++ src/Components/gridView.js
@@ -8,16 +8,49 @@ class BlogGrid extends React.Component {
     super(props);
     this.state = {
       posts: []
-    }
-  }
+    };
+}
+//  static contextType = UserContext;
+
+// componentDidMount() {
+//   // console.log('Received values of form: ', values);
+//   // const { confirm, ...data } = values;  // ignore the 'confirm' value in data sent
+//   // console.log(`logging in email: ${email}`)
+
+//   fetch('http://localhost:5000/api/dogs/',{
+//     // credentials: 'include',
+//     method: 'GET',
+//     body:JSON.stringify(data)
+//     // headers: {
+//     //   'Content-Type': 'application/json',
+//     // }
+//   })
+//   // .then(response => response.status())
+//   .then(response => response.json())
+//   .then(data => {
+//     this.setState({ posts: data })
+//       // this.context.login(data);
+//   })
+//   .catch(error => {
+//       // TODO: show nicely formatted error message
+//       console.log("Error fetching dogs");
+//   });
+// };
 
   componentDidMount() {
-    this.setState({
-      posts: require('../data/dogs.json')
+    fetch('http://localhost:5000/api/dogs')
+    // .then(response => response.status())
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      this.setState({ posts: data })
     })
-  }
+    .catch(err => console.log("Error fetching articles"));
 
+  }
   render() {
+    // return <h1>Data Loading </h1>
+    // console.log(this.state.data.length); 
     if (!this.state.posts.length) {
       return <h3>Loading posts...</h3>
     }
@@ -25,10 +58,9 @@ class BlogGrid extends React.Component {
     // to create an array of React elements to be rendered
     const cardList = this.state.posts.map(post => {
       return (
-        <div style={{padding:"10px"}} key={post.dogID}>
-          <Col span={6}>
-            <PostCard {...post} />  // look up the ‘spread’ or ‘rest’ ES6 operator
-					   // if you don’t know this syntax: {...post}
+        <div style={{padding:"10px"}} key={post._id}>
+          <Col span={2}>
+            <PostCard {...post} />  
           </Col>
         </div>
       )

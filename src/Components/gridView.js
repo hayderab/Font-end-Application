7fe495caef82660f39ce69949ab09a00d9970fe2
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-05 02:45:14 +0100

    added pagination

--- src/Components/gridView.js
+++ src/Components/gridView.js
@@ -1,59 +1,66 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 import PostCard from './cardview';
+import { Pagination, Table, Button, Space } from 'antd';
+import {LoadingOutlined} from '@ant-design/icons';
 
-class BlogGrid extends React.Component {
+import { LocalConvenienceStoreOutlined } from '@material-ui/icons';
+import SearchBar from "./searchbar.js";
+
+class GridView extends React.Component {
+
+  pageSatate = { page: 0 };
 
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
-    };
+      posts: [],
+      current:0,
+      limit:0
+        };
 }
-//  static contextType = UserContext;
 
-// componentDidMount() {
-//   // console.log('Received values of form: ', values);
-//   // const { confirm, ...data } = values;  // ignore the 'confirm' value in data sent
-//   // console.log(`logging in email: ${email}`)
+  onChange = page => {
+    // console.log(page);
+    // console.log(this.state.test);
+    this.componentDidMount(page);
+    this.setState({
+      current: page,
+      test:page
+    });
+  };
 
-//   fetch('http://localhost:5000/api/dogs/',{
-//     // credentials: 'include',
-//     method: 'GET',
-//     body:JSON.stringify(data)
-//     // headers: {
-//     //   'Content-Type': 'application/json',
-//     // }
-//   })
-//   // .then(response => response.status())
-//   .then(response => response.json())
-//   .then(data => {
-//     this.setState({ posts: data })
-//       // this.context.login(data);
-//   })
-//   .catch(error => {
-//       // TODO: show nicely formatted error message
-//       console.log("Error fetching dogs");
-//   });
-// };
-
-  componentDidMount() {
-    fetch('http://localhost:5000/api/dogs')
-    // .then(response => response.status())
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      this.setState({ posts: data })
-    })
-    .catch(err => console.log("Error fetching articles"));
+  componentDidMount(page) {
+      this.limit = 3;
+      console.log(page);
+      // const url = 'http://localhost:5000/api/dogs/?page='+this.page+'&limit=2&type=bulldog&name=luccy&avilable=false'
+      const url = `http://localhost:5000/api/dogs/?page=${page}&limit=${this.limit}`
+      fetch(url)
+      // .then(response => response.status())
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ posts: data })
+      })
+      .catch(err => console.log("Error fetching articles"));
 
   }
   render() {
-    // return <h1>Data Loading </h1>
     // console.log(this.state.data.length); 
     if (!this.state.posts.length) {
-      return <h3>Loading posts...</h3>
+      return <LoadingOutlined />
+
     }
+    // <Col>
+    //       <>
+    //       <Space style={{ marginBottom: 16 }}>
+    //         <Button onClick={this.setAgeSort}>Sort age</Button>
+    //         <Button onClick={this.clearFilters}>Clear filters</Button>
+    //         <Button onClick={this.clearAll}>Clear filters and sorters</Button>
+    //       </Space>
+    //     </>
+    //      </Col>
+
     // the next line does the Array.map() operation on the posts
     // to create an array of React elements to be rendered
     const cardList = this.state.posts.map(post => {
@@ -62,15 +69,38 @@ class BlogGrid extends React.Component {
           <Col span={2}>
             <PostCard {...post} />  
           </Col>
+          
         </div>
       )
     });
     return (
-      <Row type="flex" justify="space-around">
+      // <Row type="flex" justify="space-around">
+      //   {cardList}
+      //   <Pagination current={this.state.current} onChange={this.onChange} total={30} />
+      // </Row>
+      <div>
+        <Col>
+        <>
+
+           <Space style={{ marginBottom: 16 }}>
+
+             <Button onClick={this.setAgeSort}>Sort Date</Button>
+             <Button onClick={this.clearFilters}>Clear filters</Button>
+             <Button onClick={this.clearAll}>Clear filters and sorters</Button>
+             <SearchBar/>
+
+           </Space>
+         </>
+        </Col>
+        <Row type="flex" justify="space-around">
         {cardList}
-      </Row>
+        </Row>
+        <Col>
+        <Pagination current={this.state.current} onChange={this.onChange} total={30} />
+        </Col>
+      </div>
     );
   }
 }
 
-export default BlogGrid;
+export default GridView;

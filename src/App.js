ce901c30306824123f97c5346541cf552b3608cd
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-18 16:13:59 +0100

    added favourite link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import signup from './Components/singup';
 import home from "./Components/home";
 import login from "./Components/login";
 import addDogs from "./Components/addDogs"
+import favView from "./Components/favourites/favView"
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import UserContext from './contexts/user';
 
@@ -16,16 +18,16 @@ export class App extends Component {
     super(props);
     // this.login();
     this.state = {
-      user: {loggedIn: false}
+      user: { loggedIn: false }
     }
 
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
   }
-  componentDidMount(){
+  componentDidMount() {
     this.login();
   }
-   login() {
+  login() {
     console.log("User is now being set on the context");
     // console.log("getting user data from state", user)
     console.log("this is the database")
@@ -41,27 +43,27 @@ export class App extends Component {
       .then(user => {
         // console.log(user.sigupcode);
         console.log("user Loged in", user);
-        if(user.sigupcode == true && user.login == true){
+        if (user.sigupcode == true && user.login == true) {
           console.log("user Loged in", true);
-          this.state.user = {loggedIn: true}
-          this.setState({ 
-            user:{
+          this.state.user = { loggedIn: true }
+          this.setState({
+            user: {
               sigupcode: true,
               loggedIn: true
             }
           });
-        }else if (user.login == true){
-          this.state.user = {loggedIn: true}
-          this.setState({ 
-            user:{
+        } else if (user.login == true) {
+          this.state.user = { loggedIn: true }
+          this.setState({
+            user: {
               sigupcode: false,
               loggedIn: true
             }
           })
-        }else{
-          this.state.user = {loggedIn: true}
-          this.setState({ 
-            user:{
+        } else {
+          this.state.user = { loggedIn: true }
+          this.setState({
+            user: {
               sigupcode: false,
               loggedIn: false
             }
@@ -94,7 +96,7 @@ export class App extends Component {
       });
   }
   render() {
-      const context = {
+    const context = {
       user: this.state.user,
       login: this.login,
       logout: this.logout
@@ -106,8 +108,10 @@ export class App extends Component {
           <Content>
             <Route path='/' exact component={home} />
             <Route path='/add' component={addDogs} />
+            {/* <Route path='/add' children={addDogs} /> */}
             <Route path='/sign-up' component={signup} />
             <Route path='/signin' component={login} />
+            <Route path='/favourite' component={favView} />
             {/* <Route path='/logout'/> */}
           </Content>
         </Router>

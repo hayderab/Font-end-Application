f38b935843d3d29d24f75f578b7e0978935b6b2c
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-18 16:15:20 +0100

    fetch request for adding dogs to favourites, deleting dogs

--- src/Components/cardview.js
+++ src/Components/cardview.js
@@ -2,14 +2,14 @@
 // import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import { Card } from 'antd';
-import { EditOutlined, SettingOutlined } from '@ant-design/icons';
+import { EditOutlined, SettingOutlined, DeleteOutlined } from '@ant-design/icons';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 // import { makeStyles } from '@material-ui/core/styles';
 // import IconButton from '@material-ui/core/IconButton';
 import { Modal, Button, Form, Space, Select, Input, InputNumber } from 'antd';
 import UserContext from '../contexts/user';
 
-import React, { useState, Context} from 'react';
+import React, { useState, Context } from 'react';
 
 const { Meta } = Card;
 
@@ -28,8 +28,6 @@ const validateMessages = {
   },
 
 };
-
-
 
 
 class CardView extends React.Component {
@@ -61,12 +59,47 @@ class CardView extends React.Component {
     });
   };
 
+  addFav  = () =>{
+    alert(this.props._id);
+    fetch(`http://localhost:5000/api/users/addtofav/${this.props._id}`,
+     { method: 'POST',
+       credentials:"include"})
+      .then(data => {
+      // TODO: display success message and/or redirect
+      console.log(data.status);
+      if (data.status === 403) {
+        alert("permission denied")
+      } else {
+        alert("Dog added to favourite")
+      }
+    })
+    .catch(error => {
+      alert(`Error: error}`);
+    });
+  }
+
   // componentDidUpdate(prevProps) {
   //   // Typical usage (don't forget to compare props):
   //   if (this.props._id !== prevProps._id) {
   //     console.log(this.props._id)
   //   }
   // }
+  onDeleteClick = () => {
+    // DELETE using /api/v1/dogs/:id
+    const url = 'http://localhost:5000/api/dogs/delete/' + this.props._id;
+    const response = fetch(url, {
+      credentials: 'include',
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    // Store response in json format
+    console.log(response);
+    alert(this.props.name +  " has been deleted!");
+    window.location.href = "/dogs";
+  }
 
   onFinish = values => {
     console.log('Received values of form: ', values);
@@ -80,12 +113,6 @@ class CardView extends React.Component {
         'Content-Type': 'application/json'
       }
     })
-      // .then(function(data) {
-      //   console.log(".................",data.status);
-      //   if (data.stus  == 403){alert("permission Denied")}
-      //   return data.json();
-      // })
-      // .then(response => response.JSON(message)})
       .then(data => {
         // TODO: display success message and/or redirect
         console.log(data.status);
@@ -94,8 +121,6 @@ class CardView extends React.Component {
         } else {
           alert("Dogs updated")
         }
-
-
       })
       .catch(error => {
         alert(`Error: error}`);
@@ -104,193 +129,147 @@ class CardView extends React.Component {
 
 
   render() {
-    const loggedIn  = this.context.user.loggedIn;
+    const loggedIn = this.context.user.loggedIn;
     const sigupCode = this.context.user.sigupcode;
 
-     if(!loggedIn){
+    if (!loggedIn) {
       return (
         <div className="site-card-border-less-wrapper">
           <Card title="Card title" hoverable bordered={false}
-  
             style={{ width: 300 }}
             cover={
               <img
                 onClick={this.clickCard}
                 alt="example"
                 src="https://images.unsplash.com/photo-1491604612772-6853927639ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" />
-  
-  
             }
-            actions={[
-              // <SettingOutlined key="setting" />,
-              // <EditOutlined key="edit" onClick={this.showModal} />,
-              <FavoriteIcon key="fav" />
-  
-              // <EllipsisOutlined key="ellipsis" />,
-            ]}
           >
             <h1>{this.props.name}</h1>
             <p>{this.props.type}</p>
             <p>{this.props.location}</p>
             <p>{this.props.avilable.toString()}</p>
             <p>{this.props.dateUpdated}</p>
-  
-            <>
-              <Modal title="Update Dogs" visible={this.state.visible} onOk={this.hideModal} onCancel={this.hideModal}>
-                <Form {...layout} name="nest-messages" onFinish={this.onFinish} validateMessages={validateMessages}>
-                  <Form.Item name="name" label="Name" rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="type" label="Type" rules={[{ type: "string" }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="location" label="Select">
-                    <Select>
-                      <Select.Option value="Coventry">Coventry</Select.Option>
-                      <Select.Option value="London">London</Select.Option>
-                      <Select.Option value="Birmingham">Birmingham</Select.Option>
-                      <Select.Option value="Luton">Luton</Select.Option>
-                      <Select.Option value="Bradford">Bradford</Select.Option>
-                    </Select>
-                  </Form.Item>
-                  <Form.Item name="avilable" label="Avilable" rules={[{ type: "string" }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                    <Button type="primary" htmlType="submit"  >
-                      Submit
-          </Button>
-                  </Form.Item>
-                </Form>
-              </Modal>
-            </>
           </Card>
         </div>
-  
       );
-     } else if(loggedIn == true && sigupCode == true){
+    } else if (loggedIn == true && sigupCode == true) {
       return (
         <div className="site-card-border-less-wrapper">
           <Card title="Card title" hoverable bordered={false}
-  
-            style={{ width: 300 }}
-            cover={
-              <img
-                onClick={this.clickCard}
-                alt="example"
-                src="https://images.unsplash.com/photo-1491604612772-6853927639ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" />
-  
-  
-            }
-            actions={[
-              <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" onClick={this.showModal} />,
-              <FavoriteIcon key="fav" />
-  
-              // <EllipsisOutlined key="ellipsis" />,
-            ]}
-          >
-            <h1>{this.props.name}</h1>
-            <p>{this.props.type}</p>
-            <p>{this.props.location}</p>
-            <p>{this.props.avilable.toString()}</p>
-            <p>{this.props.dateUpdated}</p>
-  
-            <>
-              <Modal title="Update Dogs" visible={this.state.visible} onOk={this.hideModal} onCancel={this.hideModal}>
-                <Form {...layout} name="nest-messages" onFinish={this.onFinish} validateMessages={validateMessages}>
-                  <Form.Item name="name" label="Name" rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="type" label="Type" rules={[{ type: "string" }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="location" label="Select">
-                    <Select>
-                      <Select.Option value="Coventry">Coventry</Select.Option>
-                      <Select.Option value="London">London</Select.Option>
-                      <Select.Option value="Birmingham">Birmingham</Select.Option>
-                      <Select.Option value="Luton">Luton</Select.Option>
-                      <Select.Option value="Bradford">Bradford</Select.Option>
-                    </Select>
-                  </Form.Item>
-                  <Form.Item name="avilable" label="Avilable" rules={[{ type: "string" }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                    <Button type="primary" htmlType="submit"  >
-                      Submit
-          </Button>
-                  </Form.Item>
-                </Form>
-              </Modal>
-            </>
-          </Card>
-        </div>
-  
-      );
-     }
-     return(
-        <div className="site-card-border-less-wrapper">
-          <Card title="Card title" hoverable bordered={false}
-  
-            style={{ width: 300 }}
-            cover={
-              <img
-                onClick={this.clickCard}
-                alt="example"
-                src="https://images.unsplash.com/photo-1491604612772-6853927639ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" />
-  
-            }
-            actions={[
-              <SettingOutlined key="setting" />,
-              // <EditOutlined key="edit" onClick={this.showModal} />,
-              <FavoriteIcon key="fav" />
-  
-              // <EllipsisOutlined key="ellipsis" />,
-            ]}
-          >
-            <h1>{this.props.name}</h1>
-            <p>{this.props.type}</p>
-            <p>{this.props.location}</p>
-            <p>{this.props.avilable.toString()}</p>
-            <p>{this.props.dateUpdated}</p>
-            <>
-              <Modal title="Update Dogs" visible={this.state.visible} onOk={this.hideModal} onCancel={this.hideModal}>
-                <Form {...layout} name="nest-messages" onFinish={this.onFinish} validateMessages={validateMessages}>
-                  <Form.Item name="name" label="Name" rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="type" label="Type" rules={[{ type: "string" }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="location" label="Select">
-                    <Select>
-                      <Select.Option value="Coventry">Coventry</Select.Option>
-                      <Select.Option value="London">London</Select.Option>
-                      <Select.Option value="Birmingham">Birmingham</Select.Option>
-                      <Select.Option value="Luton">Luton</Select.Option>
-                      <Select.Option value="Bradford">Bradford</Select.Option>
-                    </Select>
-                  </Form.Item>
-                  <Form.Item name="avilable" label="Avilable" rules={[{ type: "string" }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                    <Button type="primary" htmlType="submit"  >
-                      Submit
-          </Button>
-                  </Form.Item>
-                </Form>
-              </Modal>
-            </>
-          </Card>
-        </div>
-  
-      );
-     
 
-   
+            style={{ width: 300 }}
+            cover={
+              <img
+                onClick={this.clickCard}
+                alt="example"
+                // src="http://localhost:5000/uploads/1618693523270dogs.jpg" 
+                 src= {`http://localhost:5000/${this.props.imageUrl}`}
+
+                />
+
+            }
+            actions={[
+              <EditOutlined key="edit" onClick={this.showModal} />,
+              <DeleteOutlined key="delete" DeleteOutlined onClick={this.onDeleteClick}/>,
+              <FavoriteIcon key="fav" />
+              // <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <h1>{this.props.name}</h1>
+            <p>{this.props.type}</p>
+            <p>{this.props.location}</p>
+            <p>{this.props.avilable.toString()}</p>
+            <p>{this.props.dateUpdated}</p>
+
+            <>
+              <Modal title="Update Dogs" visible={this.state.visible} onOk={this.hideModal} onCancel={this.hideModal}>
+                <Form {...layout} name="nest-messages" onFinish={this.onFinish} validateMessages={validateMessages}>
+                  <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+                    <Input />
+                  </Form.Item>
+                  <Form.Item name="type" label="Type" rules={[{ type: "string" }]}>
+                    <Input />
+                  </Form.Item>
+                  <Form.Item name="location" label="Select">
+                    <Select>
+                      <Select.Option value="Coventry">Coventry</Select.Option>
+                      <Select.Option value="London">London</Select.Option>
+                      <Select.Option value="Birmingham">Birmingham</Select.Option>
+                      <Select.Option value="Luton">Luton</Select.Option>
+                      <Select.Option value="Bradford">Bradford</Select.Option>
+                    </Select>
+                  </Form.Item>
+                  <Form.Item name="avilable" label="Avilable" rules={[{ type: "string" }]}>
+                    <Input />
+                  </Form.Item>
+                  <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                    <Button type="primary" htmlType="submit"  >
+                      Submit
+          </Button>
+                  </Form.Item>
+                </Form>
+              </Modal>
+            </>
+          </Card>
+        </div>
+      );
+    }
+    return (
+      <div className="site-card-border-less-wrapper">
+        <Card title="Card title" hoverable bordered={false}
+
+          style={{ width: 300 }}
+          cover={
+            <img
+              onClick={this.clickCard}
+              alt="example"
+              src="https://images.unsplash.com/photo-1491604612772-6853927639ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" />
+
+          }
+          actions={[
+            <SettingOutlined key="setting" />,
+            <FavoriteIcon key="fav"  onClick={this.addFav}/>
+          ]}
+        >
+          <h1>{this.props.name}</h1>
+          <p>{this.props.type}</p>
+          <p>{this.props.location}</p>
+          <p>{this.props.avilable.toString()}</p>
+          <p>{this.props.dateUpdated}</p>
+
+          {/* <>
+            <Modal title="Update Dogs" visible={this.state.visible} onOk={this.hideModal} onCancel={this.hideModal}>
+              <Form {...layout} name="nest-messages" onFinish={this.onFinish} validateMessages={validateMessages}>
+                <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+                  <Input />
+                </Form.Item>
+                <Form.Item name="type" label="Type" rules={[{ type: "string" }]}>
+                  <Input />
+                </Form.Item>
+                <Form.Item name="location" label="Select">
+                  <Select>
+                    <Select.Option value="Coventry">Coventry</Select.Option>
+                    <Select.Option value="London">London</Select.Option>
+                    <Select.Option value="Birmingham">Birmingham</Select.Option>
+                    <Select.Option value="Luton">Luton</Select.Option>
+                    <Select.Option value="Bradford">Bradford</Select.Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item name="avilable" label="Avilable" rules={[{ type: "string" }]}>
+                  <Input />
+                </Form.Item>
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                  <Button type="primary" htmlType="submit"  >
+                    Submit
+                </Button>
+                </Form.Item>
+              </Form>
+            </Modal>
+          </> */}
+        </Card>
+      </div>
+
+    );
   }
 
 }
